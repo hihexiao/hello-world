532d7990aeb688d586e9b921ac8c8cbc56f15f25
hihexiao <56291916+hihexiao@users.noreply.github.com>
2019-11-26 14:24:25 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<curl/curl.h>
+
+int main()
+{
+  CURL *curl;
+  CURL code res;
+  
+  curl=curl_easy_init();
+  if(curl){
+   curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip1.net/");
+   res=curl_easy_perfom(curl);
+   if(res!=CURLE_OK)
+    fprintf(stderr,"curl_easy_perform() failed:%s\n";
+          curl_easy_cleanup(curl);
+    curl_easy_cleanup(curl);
+ }
+ return0;
+ }
+
